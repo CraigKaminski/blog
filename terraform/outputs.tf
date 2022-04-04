a80e6cafe469f95ee52580007239c7ae5bc3a38b
Craig Kaminski <craig.j.kaminski@gmail.com>
2022-04-04 14:27:42 -0400

    Fix typo

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "s3_bucket_name" {
-  discription = "Name of S3 bucket hosting the app"
+  description = "Name of S3 bucket hosting the app"
   value       = resource.aws_s3_bucket.hosting.id
 }
